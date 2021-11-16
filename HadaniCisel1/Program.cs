592c80dfd586a2f84b9ce287405627400c61b9c6
petru.stepan <petru.stepan@D20-05>
2021-11-16 11:29:54 +0100

    gunaka

--- HadaniCisel1/Program.cs
+++ HadaniCisel1/Program.cs
@@ -6,7 +6,52 @@ namespace HadaniCisel1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("quokka");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Random random = new Random();
+            int nahodne_cislo = random.Next(0, 100);
+            double tip;
+            bool neuhodl = true;
+            while (true)
+            {
+                Console.WriteLine("Hádání čísla");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Myslím si číslo od 0 do 100");
+                while (neuhodl)
+                {
+                    Console.WriteLine("Tipni si číslo");
+                    double.TryParse(Console.ReadLine(), out tip);
+                    if (tip < nahodne_cislo)
+                    {
+                        Console.WriteLine("To je málo :( ");
+                        Console.WriteLine();
+                    }
+                    else if (tip > nahodne_cislo)
+                    {
+                        Console.WriteLine("To je moc :( ");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Trefa");
+                        neuhodl = false;
+                    }
+
+
+
+
+
+
+
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
