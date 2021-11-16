30cd0b9d470868d52c1800e85882760c9ee55d0f
petru.stepan <petru.stepan@D20-05>
2021-11-16 10:09:50 +0100

    Přidejte soubory projektu.

---
+++ HadaniCisel1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HadaniCisel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quokka");
+        }
+    }
+}
